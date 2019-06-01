52d232b22374460c1c2cc99282b242d5693729dd
Vlia1102 <31473199@qq.com>
2019-06-01 10:23:13 +0800

    redo

---
+++ src/main/java/com/zz/comindex/ComindexApplication.java
@@ -0,0 +1,13 @@
+package com.zz.comindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComindexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComindexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zz/comindex/ComindexApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zz.comindex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComindexApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
